84ca08e11c19d38a11b2d352d1f1bdbbf1d7fe0d
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2014-10-21 16:50:14 +0200

    Added Constructor

--- Bicycle.java
+++ Bicycle.java
@@ -6,6 +6,15 @@ public class Bicycle {
     public int gear;
     public int speed;
 
+        
+    // the Bicycle class has
+    // one constructor
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+
     // the Bicycle class has
     // four methods
     public void setCadence(int newValue) {
