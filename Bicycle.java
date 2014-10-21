961e2fd3580af560d9ee2253f2fb52d21caa6621
Benedikt Christoph Wolters <benedikt.wolters@rwth-aachen.de>
2014-10-21 16:43:33 +0200

    Added Getters

--- Bicycle.java
+++ Bicycle.java
@@ -6,4 +6,23 @@ public class Bicycle {
     public int gear;
     public int speed;
 
+    // the Bicycle class has
+    // four methods
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+        
+
 }
